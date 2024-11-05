a5ae0f76ed3977aa38b9c1bab505d8b5e0f71363
GMGustafson <gustafsg@email.sc.edu>
2024-11-05 15:06:32 -0500

    delete stuff

--- mpt-website/src/components/Header.js
+++ mpt-website/src/components/Header.js
@@ -3,9 +3,9 @@ import "../css/Header.css";
 const Header = () => { 
     return (
         <header class="columns"> 
-            <img id="logo1" src="MPT/MPTimages/logo.jpg" alt="Logo 1"></img>
+            <img id="logo1" src="MPTimages/logo.jpg" alt="Logo 1"></img>
             <h1 id="header-name"> Mackin Pallet & Trucking Inc. </h1>
-            <img id="logo2" src="MPT/MPTimages/logo.jpg" alt="Logo 2"></img>
+            <img id="logo2" src="MPTimages/logo.jpg" alt="Logo 2"></img>
         </header>
     ); 
 };
